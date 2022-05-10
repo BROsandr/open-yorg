b5298092a3dac97b67388363c70f6ce4707663e2
BROsandr <sasha_xarlamov@mail.ru>
2022-05-10 23:13:40 +0300

    change

--- src/SConscript.py
+++ src/SConscript.py
@@ -3,7 +3,7 @@ include = ['../include/']
 libs = ['sfml-graphics', 'sfml-system', 'sfml-window']
 cxxflags = ['-Wall' ,'-Wextra' ,'-pedantic' ,'-std=c++17' ,'-O2' ,'-Wshadow' ,'-Wformat=2' ,'-Wfloat-equal' ,'-Wconversion' ,'-Wcast-qual' ,'-Wcast-align' ,'-D_GLIBCXX_DEBUG' ,'-D_GLIBCXX_DEBUG_PEDANTIC' ,'-D_FORTIFY_SOURCE=2' ,'-fsanitize=address' ,'-fsanitize=undefined', '-fno-sanitize-recover', '-fstack-protector']
 linkflags =['-D_GLIBCXX_DEBUG' ,'-D_GLIBCXX_DEBUG_PEDANTIC' ,'-D_FORTIFY_SOURCE=2' ,'-fsanitize=address' ,'-fsanitize=undefined', '-fno-sanitize-recover', '-fstack-protector']
-env = Environment(CPPPATH=include, LINKFLAGS = linkflags, LIBS=libs, CXX = 'clang++', CXXCOMSTR = "Compiling $SOURCE", LINKCOMSTR = "Linking $TARGET"  ,CXXFLAGS = cxxflags)
+env = Environment(CPPPATH=include, LINKFLAGS = linkflags, LIBS=libs, CXX = '/home/alex/Downloads/clang+llvm-14.0.0-x86_64-linux-gnu-ubuntu-18.04/bin/clang++', CXXCOMSTR = "Compiling $SOURCE", LINKCOMSTR = "Linking $TARGET"  ,CXXFLAGS = cxxflags)
 env['ENV']['TERM'] = os.environ['TERM']
 env.Decider('MD5-timestamp')
 env.Tool('compilation_db')
